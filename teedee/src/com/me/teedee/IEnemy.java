9c4c4b15fdf6b514bf5129033e99083cee608499
Fridgeridge <d.michaelsson@gmail.com>
2014-03-27 21:01:02 +0100

    Filled IEnemy and Path, added BasicEnemy
    Added methods to IEnemy and Path.  Classes are not yet complete. Added
    new class BasicEnemy which implements IEnemy, method bodies are not yet
    complete.

--- teedee/src/com/me/teedee/IEnemy.java
+++ teedee/src/com/me/teedee/IEnemy.java
@@ -1,5 +1,70 @@
 package com.me.teedee;
-
+/**
+ * The interface of the enemy class. Provides the body to be 
+ * used and implemented by enemy units.
+ * 
+ * @author Fridgeridge
+ *
+ */
 public interface IEnemy {
 
+		
+	
+	/**
+	 * Enemy takes damage when invoked
+	 */
+	public void takeDamage(int damage);
+	
+	
+	/**
+	 * Enemy takes damage and gets a status effect when invoked
+	 */
+	public void takeDamage(int damage, Status s);
+	
+	
+	/**
+	 * Enemy moves forward in the path
+	 */
+	public void move();
+	
+	/**
+	 * Enemy dies,awards player money and 
+	 * disappears from the map.
+	 */
+	public void die();
+	
+	/**
+	 * Method that checks if the enemy has end of path next move
+	 * 
+	 * @return true if end of path is the next move 
+	 */
+	
+	public boolean hasNextEnd();
+	
+	/**
+	 * Gets the money reward from an enemy
+	 * 
+	 * @return Reward money 
+	 */
+	public Reward getReward();
+	
+	/**
+	 * Sets a status effect to the enemy unit
+	 * @param s The status effect to be set to the enemy
+	 */
+	public void setStatusEffect(Status s);
+	
+	/**
+	 * Gets the current status effect from the enemy unit
+	 * 
+	 */
+	public Status getStatusEffect();
+	
+	/**
+	 * Method that returns information about the enemy unit
+	 * @return A string message containing information about the enemy unit
+	 */
+	public String toString();
+	
+	
 }
--- teedee/src/com/me/teedee/Path.java
+++ teedee/src/com/me/teedee/Path.java
@@ -1,5 +1,37 @@
 package com.me.teedee;
 
-public class Path {
+//import java.awt.Point;
 
+
+/**
+ * Describes the path that the enemy units will take when they travel through the map. 
+ * 
+ * @author dmich_000
+ *
+ */
+public class Path {
+	
+	/**
+	 * The coordinates that describes the path through the map from a top-down perspective
+	 */
+	private int[] xPos,yPos; 
+	
+	//private Point[] pos;
+	
+	/**
+	 * Method that returns the x-coordinates
+	 * @return the array of x-coordinates
+	 */
+	public int[] getXPos(){
+		return xPos;
+	}
+	
+	/**
+	 * Method that returns the y-coordinates
+	 * @return the array of y-coordinates
+	 */
+	public int[] getYPos(){
+		return yPos;
+	}
+	
 }
