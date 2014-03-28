805bac46aab48a02f6aa1759091df9ffa0dc1b2d
Jacob Genander <jacob.genander@hotmail.com>
2014-03-28 13:51:41 +0100

    Implemented Map and Status classes

--- teedee/src/com/me/teedee/Map.java
+++ teedee/src/com/me/teedee/Map.java
@@ -1,5 +1,91 @@
 package com.me.teedee;
 
+import java.util.Iterator;
+import java.util.List;
+
+/**
+@author Jacob Genander
+*/
 public class Map {
+	private List<Tower> towers;
+	private List<Wave> waves;
+	private Path path;
+	private Player player;
+	private Wave currentWave;
+	
+	/**
+	 * Constructs a map containing the given waves, path and player
+	 * @param waves a list containing wave objects which are going to be used during the game
+	 * @param path the path which enemies will travel on
+	 * @param player the object representing the player of the game
+	 */
+	public Map(List<Wave> waves, Path path, Player player){
+		this.waves = waves;
+		this.path = path;
+		this.player = player;
+		if(!this.waves.isEmpty()){
+			this.currentWave = this.waves.get(0);
+		}
+	}
+	
+	/**
+	 * Returns a list containing all the enemies in the current wave
+	 * @return a list containing all the enemies in the current wave
+	 */
+	public List<IEnemy> getEnemies(){
+		return currentWave.getEnemies();
+	}
+	
+	/**
+	 * Returns a list of towers that are currently on the map
+	 * @return a list of towers that are currently on the map
+	 */
+	public List<Tower> getTowers(){
+		return towers;
+	}
+	
+	/**
+	 * Builds a tower on a position
+	 * @param tower the tower to be built on the map
+	 * @param position the position on the map which the tower will be built on
+	 */
+	public void buildTower(Tower tower, Position position){
+		tower.setPosition(position);
+		this.towers.add(tower);
+	}
+	
+	/**
+	 * Returns the map's path
+	 * @return the path of the map
+	 */
+	public Path getPath(){
+		return path;
+	}
+	
+	/**
+	 * Returnss the game's player
+	 * @return the player currently playing the game
+	 */
+	public Player getPlayer(){
+		return player;
+	}
+	
+	/**
+	 * Replaces the game's player
+	 * @param player the player who will be playing the game
+	 */
+	public void setPlayer(Player player){
+		this.player = player;
+	}
+	
+	/**
+	 * Setting the current wave of enemies to the next wave in the list of waves
+	 */
+	public void nextWave(){
+		Iterator<Wave> waveIterator = waves.iterator();
+		if(waveIterator.hasNext()){
+			currentWave = waveIterator.next();
+		}
+	}
 
 }
--- teedee/src/com/me/teedee/Status.java
+++ teedee/src/com/me/teedee/Status.java
@@ -1,5 +1,59 @@
 package com.me.teedee;
+/**
+ * 
+ * @author Jacob Genander
+ *
+ * A class representing the status of an enemy. The status affects the enemy by 
+ * changing its speed, and may also cause damage during a period of time.
+ */
 
-public class Status {
+public abstract class Status {
+	private double speed;
+	private double damagePerSecond;
+	private double influenceTime;
+	
+	/**
+	 * Constructs a default status
+	 */
+	public Status(){
+		speed = 1;
+		damagePerSecond = 0;
+		influenceTime = 0;
+	}
+	
+	/**
+	 * Constructs a status with the given speed, damage per second and time of imfluence
+	 * @param speed the new speed of the enemy
+	 * @param damagePerSecond the damage the enemy will take per second during the status time of influence
+	 * @param influenceTime the time in milliseconds that the status will have influence on the enemy
+	 */
+	public Status(double speed, double damagePerSecond, double influenceTime){
+		this.speed = speed;
+		this.damagePerSecond = damagePerSecond;
+		this.influenceTime = influenceTime;
+	}
 
+	/**
+	 * 
+	 * @return the status' applied speed
+	 */
+	public double getSpeed(){
+		return speed;
+	}
+	
+	/**
+	 * 
+	 * @return the status' applied damage per second
+	 */
+	public double getDamagePerSecond(){
+		return damagePerSecond;
+	}
+	
+	/**
+	 * 
+	 * @return the status' applied time of influence
+	 */
+	public double getInfluenceTime(){
+		return influenceTime;
+	}
 }
