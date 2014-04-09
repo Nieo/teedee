ae90b7e38ba92b654f007123e7f2a2d1fe021e33
Jacob Genander <jacob.genander@hotmail.com>
2014-04-09 17:05:00 +0200

    Changed the timer in Tower so that it doesn't pause the rendering thread. Also added some initialization values to BasicTowers.

--- teedee-desktop/src/com/me/teedee/Main.java
+++ teedee-desktop/src/com/me/teedee/Main.java
@@ -2,16 +2,15 @@ package com.me.TeeDee;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.me.teedee.TeeDeeGame;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "TeeDee";
-		//cfg.useGL20 = false;
-		cfg.width = 960;
-		cfg.height = 720;
+		cfg.title = "teedee";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
 		
-		new LwjglApplication(new TeeDeeGame(), cfg);
+		new LwjglApplication(new TeeDee(), cfg);
 	}
 }
