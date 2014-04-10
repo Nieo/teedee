4d84f4aefe7fa1d10bb4f12edb6693ee82db203e
Fridgeridge <d.michaelsson@gmail.com>
2014-04-10 10:06:19 +0200

    Fixed some small errors whith desktop main

--- teedee-desktop/src/com/me/teedee/Main.java
+++ teedee-desktop/src/com/me/teedee/Main.java
@@ -2,15 +2,16 @@ package com.me.TeeDee;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.me.teedee.TeeDeeGame;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "teedee";
-		cfg.useGL20 = false;
+//		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
 		
-		new LwjglApplication(new TeeDee(), cfg);
+		new LwjglApplication(new TeeDeeGame(), cfg);
 	}
 }
