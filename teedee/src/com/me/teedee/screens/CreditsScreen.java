12603c60561a15d4ab1e8fc9644b11348c0eb569
Nieo <pihl_93@hotmail.com>
2014-05-08 15:30:59 +0200

    Made a creditsscreen

--- teedee/src/com/me/teedee/screens/CreditsScreen.java
+++ teedee/src/com/me/teedee/screens/CreditsScreen.java
@@ -1,30 +1,97 @@
 package com.me.teedee.screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class CreditsScreen implements Screen {
-
+	
+	private Stage stage;
+	private SpriteBatch batch;
+	private Table table;
+	
+	private Skin skin;
+	private Texture background;
+	private Sprite bSprite;
+	
+	private Label header;
+	private Label body;
+	
+	private TextButton backButton;
+	
+	
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		bSprite.draw(batch);
+		batch.end();
+		
+		stage.act(delta);
+		stage.draw();
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+		stage.getViewport().update(width, height, true);
 
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-
+		
+		
+		skin  = new Skin(Gdx.files.internal("skin/uiskin.json"));
+		stage = new Stage();
+		
+		Gdx.input.setInputProcessor(stage);
+		
+		header = new Label("Creators of teedee", skin);
+		body = new Label("Nieo, FridgeRidge,Dannemannet,Jacob \n"
+				+ "Magni memor Ludovico XII", skin);
+		
+		backButton = new TextButton("Back", skin);
+		backButton.pad(20);
+		backButton.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y){
+				((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen());
+			}
+		});
+		
+		table = new Table(skin);
+		table.setFillParent(true);
+		
+		table.add(header).spaceBottom(20).row();
+		table.add(body).spaceBottom(20).row();
+		table.add(backButton).row();
+		
+		stage.addActor(table);
+		
+		batch = new SpriteBatch();	
+		
+		background = new Texture("data/MainMenu.png");
+		bSprite = new Sprite(background);
+		bSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
+		dispose();
 
 	}
 
@@ -42,7 +109,9 @@ public class CreditsScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
+		stage.dispose();
+		batch.dispose();
+		background.dispose();
 
 	}
 
