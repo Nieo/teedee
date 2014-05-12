afecc295f4ae98d7467eed51b21dc28324fcd25a
Fridgeridge <d.michaelsson@gmail.com>
2014-05-12 14:45:33 +0200

    Added functionality to pause and resume in MapScreen

--- teedee/src/com/me/teedee/screens/MapScreen.java
+++ teedee/src/com/me/teedee/screens/MapScreen.java
@@ -441,9 +441,9 @@ public class MapScreen implements Screen {
 				}
 				if(event.getListenerActor().equals(pauseBtn)){
 					if(MapScreen.this.m.isRunning()){
-						MapScreen.this.m.setRunning(false);
+						MapScreen.this.pause();
 					}else{
-						MapScreen.this.m.setRunning(true);
+						MapScreen.this.resume();
 					}
 				}
 
@@ -530,11 +530,14 @@ public class MapScreen implements Screen {
 	public void hide() { dispose();	}
 
 	@Override
-	public void pause() { 
+	public void pause() {
+		MapScreen.this.m.setRunning(false);
 	}
 
 	@Override
-	public void resume() { }
+	public void resume() { 
+		MapScreen.this.m.setRunning(true);
+	}
 
 	@Override
 	public void dispose() {	
